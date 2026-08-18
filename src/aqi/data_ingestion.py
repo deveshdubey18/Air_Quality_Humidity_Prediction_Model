1ac873d8f1ca526b0b1950c213e18a40c943f2b9
Devesh Dubey <314devesh11scic@gmail.com>
2026-08-18 21:21:10 +0530

    Update data_ingestion.py

--- src/aqi/data_ingestion.py
+++ src/aqi/data_ingestion.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 
 def loader():
-    df = pd.read_excel(r'C:\AQI\data\AirQualityUCI.xlsx')
+    df = pd.read_excel(r'https://github.com/deveshdubey18/Air_Quality_Humidity_Prediction_Model/raw/refs/heads/main/data/AirQualityUCI.xlsx')
     return df
 
